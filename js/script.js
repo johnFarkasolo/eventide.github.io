95d27736be2e55ae3d3eb0155f09814cc896433c
Ivan Yarovyi <farkasolo@mail.ru>
2017-07-03 13:36:06 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+	$('.collapse').on('show.bs.collapse', function(){
+		$(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
+	});
+	$('.collapse').on('hide.bs.collapse', function(){
+		$(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+	});
+	$('#myForm').validator({
+		feedback: {
+			success: 'fa fa-check-circle',
+			error: 'fa fa-times-circle'
+		}
+	});
+
+	$('.form-select').select2();
+
+});
